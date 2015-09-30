47ba7236caf5cba0447396c407d6cf2be7dbf78d
GoncaloCeia <ceia2266@hotmail.com>
2015-09-30 23:21:36 +0100

    ThreadPool
    Lab2 Exercicio 3 (Threads) MySolution

---
+++ lab2/Threads/Threads/App.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+
+delegate void ThrWork();
+namespace App{
+    class ThrPool
+    {
+        private Buffer<ThrWork> buffer;
+        private Thread[] poolThread;
+        public ThrPool(int thrNum, int bufSize)
+        {
+               poolThread = new Thread[thrNum];
+               buffer = new Buffer<ThrWork>(bufSize);	
+               for(int index = 0; index < thrNum; index++){
+                    poolThread[index] = new Thread(new ThreadStart(consumeDelegates));
+                    poolThread[index].Start();
+                }   
+
+
+        }
+
+        public void AssyncInvoke(ThrWork action)
+        {
+            buffer.produce(action);
+            // TODO
+        }
+
+        public void consumeDelegates(){
+            while(true){
+                ThrWork obj = buffer.consume();
+                obj();
+            }
+                
+        }
+    }
+
+
+    class A
+    {
+        private int _id;
+
+        public A(int id)
+        {
+            _id = id;
+        }
+
+        public void DoWorkA()
+        {
+            Console.WriteLine("A-{0}", _id);
+        }
+    }
+
+
+    class B
+    {
+        private int _id;
+
+        public B(int id)
+        {
+            _id = id;
+        }
+
+        public void DoWorkB()
+        {
+            Console.WriteLine("B-{0}", _id);
+        }
+    }
+
+    
+    class Test
+    {
+        public static void Main()
+        {
+            ThrPool tpool = new ThrPool(5, 10);
+            ThrWork work = null;
+            for (int i = 0; i < 5; i++)
+            {
+                A a = new A(i);
+                tpool.AssyncInvoke(new ThrWork(a.DoWorkA));
+                B b = new B(i);
+                tpool.AssyncInvoke(new ThrWork(b.DoWorkB));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab2/Threads/Threads/Buffer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace App {
+    class Buffer<T> {
+
+        private T[] buffer;
+        private int index;
+        private int size;
+        private int occupied;
+        private int exec;
+
+        public Buffer(int sizeofBuffer){
+            buffer = new T[sizeofBuffer];
+            size = sizeofBuffer;
+            index = occupied = exec = 0;
+        }
+
+        public void produce(T obj){
+            lock(this){
+                while(occupied == size)
+                  Monitor.Wait(this);
+                buffer[index] = obj;
+                index = ++index % size;
+                occupied++;
+            }
+        }
+
+        public T consume(){
+            T obj;
+            lock(this){
+                while(occupied == 0)
+                    Monitor.Wait(this);
+                obj = buffer[exec];
+                buffer[exec] = default(T);
+                exec = ++exec % size;
+                occupied--;
+                Monitor.Pulse(this);
+            }
+            return obj;
+        }
+
+        public void showStatus(){
+
+            for(int i = 0; i < size; i++)
+                Console.WriteLine("Buffer[{0}] = {1}", i, buffer[i]);
+
+            Console.WriteLine("size = {0} : index = {1} : occupied = {2} : exec = {3}", size, index, occupied, exec);
+        }
+
+    }
+
+
+}

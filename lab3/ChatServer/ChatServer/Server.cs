0ca817e8e1ebb1d25c306e1cb58ed02fb332618d
paulo_teixeira <paulo_teixeira_slb@hotmail.com>
2015-10-05 21:55:26 +0100

    missing files

---
+++ lab3/ChatServer/ChatServer/Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace ChatServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(8086);
+            ChannelServices.RegisterChannel(channel,false);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(ChatServerImpl),
+                "ChatServer",
+                WellKnownObjectMode.Singleton);
+            System.Console.WriteLine("--Chat--");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ lab3/ChatServer/CommonTypes/IChatServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    public interface IChatServer
+    {
+        void Register(String nickName, String Url);   
+        void sendMessage(String nickName, String message);  
+    }
+}

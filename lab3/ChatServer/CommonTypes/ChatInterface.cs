8a3d8eb10dce387e200949ca23f93f547e3847e5
rubensousadinis <ruben.sousa.dinis@gmail.com>
2015-10-05 11:02:25 +0100

    adding chat

---
+++ lab3/ChatServer/CommonTypes/ChatInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    interface IChatInterface
+    {
+        public void Register(String nickName, String Url);
+   
+
+        public void sendMessage(String nickName, String message);
+  
+    }
+}

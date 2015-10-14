03da99da68f8a72b1fb5972ed2af6818414c3638
GoncaloCeia <ceia2266@hotmail.com>
2015-10-14 23:13:02 +0100

    Create process for Publisher #8
    Proccess similar to subscriber, with an access to a Local Broker, with a
    name and url

--- SESDAD/Publisher/Program.cs
+++ SESDAD/Publisher/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using CommonTypes;
 
 namespace Publisher
 {
     class Program
     {
+        // args[0] -> Publisher name
+        // args[1] -> Publisher url
+        // args[2] -> broker url
         static void Main(string[] args)
         {
+            String name = args[0];
+            String url = args[1];
+            String brokerUrl = args[2];
+
+            char[] delimiterChars = { ':', '/' }; // "tcp://1.2.3.4:3335/pub"
+            string[] urlSplit = url.Split(delimiterChars);
+            int port = Convert.ToInt32(urlSplit[4]);
+
+            TcpChannel channel = new TcpChannel(port);
+            ChannelServices.RegisterChannel(channel, false);
+
+            Publisher publisher = new Publisher(name, url);
+            RemotingServices.Marshal(publisher, "pub", typeof(IPublisher));
+
+            Console.WriteLine("Publisher {0} running on {1}", name, url);
+
+            publisher.registerInBroker(brokerUrl);
+            Console.ReadLine();
         }
     }
 }
--- SESDAD/Publisher/Publisher.cs
+++ SESDAD/Publisher/Publisher.cs
@@ -7,15 +7,29 @@ using CommonTypes;
 
 namespace Publisher
 {
-    class Publisher: IPublisher
+    class Publisher : MarshalByRefObject, IPublisher
     {
         private List<Event> events;
         private String publisherId;
         private String url;
+        private IBroker broker;
+        public Publisher(String publisherId, String url)
+        {
+            this.publisherId = publisherId;
+            this.url = url;
+        }
+
 
         public void Publish(String topic, String content)
         {
 
+        }
+
+        public void registerInBroker(String brokerUrl)
+        {
+            Console.WriteLine("Registing in broker at {0}", brokerUrl);
+            this.broker = (IBroker)Activator.GetObject(typeof(IBroker), brokerUrl);
+            this.broker.registerSubscriber(this.url);
         }
 
 

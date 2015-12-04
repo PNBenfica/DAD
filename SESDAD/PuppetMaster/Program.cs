feb4ef3a8933fd8f0d4e6c1d7381aa9619467cb1
paulo_teixeira <paulo_teixeira_slb@hotmail.com>
2015-12-04 11:43:08 +0000

    changing script files

--- SESDAD/PuppetMaster/Program.cs
+++ SESDAD/PuppetMaster/Program.cs
@@ -18,7 +18,7 @@ namespace PuppetMaster
 
         public static void Main(string[] args)
         {
-            String filename = @"..\..\..\T3-config.txt";
+            String filename = @"..\..\..\config.txt";
             if (args.Length > 0)
                 filename = @args[0];
 
